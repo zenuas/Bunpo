7c8d9b8b739d6d215f73b0192f7661ff031b4ac6
zenuas@gmail.com <zenuas@gmail.com>
2026-04-09 22:54:08 +0900

    再起呼び出しによる足し算と掛け算のテストケース追加

--- test/CalculationTest.cs
+++ test/CalculationTest.cs
@@ -9,9 +9,9 @@ public class CalculationTest
     public static Func<string, int, (int, float)?> Spaces = Combinator.Once(Combinator.Spaces.ToOption(), _ => 0f);
 
     public static Func<string, int, (int, float)?> Add = Combinator.Once(Combinator.Char('+'), _ => 0f);
-    public static Func<string, int, (int, float)?> Sub = Combinator.Once(Combinator.Char('+'), _ => 0f);
-    public static Func<string, int, (int, float)?> Mul = Combinator.Once(Combinator.Char('+'), _ => 0f);
-    public static Func<string, int, (int, float)?> Div = Combinator.Once(Combinator.Char('+'), _ => 0f);
+    public static Func<string, int, (int, float)?> Sub = Combinator.Once(Combinator.Char('-'), _ => 0f);
+    public static Func<string, int, (int, float)?> Mul = Combinator.Once(Combinator.Char('*'), _ => 0f);
+    public static Func<string, int, (int, float)?> Div = Combinator.Once(Combinator.Char('/'), _ => 0f);
 
     [Fact]
     public void PlusTest()
@@ -21,5 +21,27 @@ public class CalculationTest
         Assert.Equal(parser("1 + 2", 0)?.Item2, 3f);
         Assert.Equal(parser("  2+3", 0)?.Item2, 5f);
         Assert.Equal(parser("45 + 56", 0)?.Item2, 101f);
+    }
+
+    [Fact]
+    public void MulTest()
+    {
+        Func<string, int, (int, float)?> lazy_term_in = null!;
+        Func<string, int, (int, float)?> lazy_term = (input, start) => lazy_term_in(input, start);
+
+        Func<string, int, (int, float)?> lazy_expr_in = null!;
+        Func<string, int, (int, float)?> lazy_expr = (input, start) => lazy_expr_in(input, start);
+
+        var factor = Number;
+        var term = Combinator.Sequence([factor, Mul, lazy_term], xs => xs[0] * xs[2]) | factor;
+        var expr = Combinator.Sequence([term, Add, lazy_expr], xs => xs[0] + xs[2]) | term;
+
+        lazy_term_in = term;
+        lazy_expr_in = expr;
+
+        Assert.Equal(expr("1+2", 0)?.Item2, 3f);
+        Assert.Equal(expr("3*4", 0)?.Item2, 12f);
+        Assert.Equal(expr("1+2*3+4", 0)?.Item2, 11f);
+        Assert.Equal(expr("1*2+3*4", 0)?.Item2, 14f);
     }
 }
