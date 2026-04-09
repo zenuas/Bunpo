1e63d36351969f443de7751d2df7d92cee445e77
zenuas@gmail.com <zenuas@gmail.com>
2026-04-09 20:06:57 +0900

    単純な足し算のテストケース追加

---
+++ test/CalculationTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+namespace Bunpo.Test;
+
+public class CalculationTest
+{
+    public static Func<string, int, (int, float)?> Number = Combinator.Once(Combinator.Digits, float.Parse);
+    public static Func<string, int, (int, float)?> Spaces = Combinator.Once(Combinator.Spaces.ToOption(), _ => 0f);
+
+    public static Func<string, int, (int, float)?> Add = Combinator.Once(Combinator.Char('+'), _ => 0f);
+    public static Func<string, int, (int, float)?> Sub = Combinator.Once(Combinator.Char('+'), _ => 0f);
+    public static Func<string, int, (int, float)?> Mul = Combinator.Once(Combinator.Char('+'), _ => 0f);
+    public static Func<string, int, (int, float)?> Div = Combinator.Once(Combinator.Char('+'), _ => 0f);
+
+    [Fact]
+    public void PlusTest()
+    {
+        var parser = Combinator.Sequence([Spaces, Number, Spaces, Add, Spaces, Number], xs => xs[1] + xs[5]);
+
+        Assert.Equal(parser("1 + 2", 0)?.Item2, 3f);
+        Assert.Equal(parser("  2+3", 0)?.Item2, 5f);
+        Assert.Equal(parser("45 + 56", 0)?.Item2, 101f);
+    }
+}
