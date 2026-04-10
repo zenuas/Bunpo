663977e5b2f31bc1dc344ad6c37b9f371f7bbdd7
zenuas@gmail.com <zenuas@gmail.com>
2026-04-10 11:37:32 +0900

    LazyParserのコンストラクタを設定

--- src/LazyParser.cs
+++ src/LazyParser.cs
@@ -2,8 +2,13 @@
 
 namespace Bunpo;
 
-public class LazyParser<T>()
+public class LazyParser<T>
 {
-    public Func<string, int, (int, T)?> Func { get; set; } = null!;
+    public Func<string, int, (int, T)?> Func { get; init; }
     public Func<string, int, (int, T)?> LazyFunc { get; set; } = null!;
+
+    public LazyParser()
+    {
+        Func = (input, length) => LazyFunc(input, length);
+    }
 }
