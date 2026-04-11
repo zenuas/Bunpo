4754d742202c28eac940914a20f135e266be6ed1
zenuas@gmail.com <zenuas@gmail.com>
2026-04-12 01:50:27 +0900

    比較用の手書きLLパーサのベンチマーク追加

--- README.md
+++ README.md
@@ -44,18 +44,23 @@ The parser compared formulas with 5 numbers and formulas with 300 numbers.
 Four arithmetic operations benchmark:  
 | Method          | Mean          | Error        | StdDev       | Gen0     | Gen1   | Allocated |
 |---------------- |--------------:|-------------:|-------------:|---------:|-------:|----------:|
-| BunpoSetup      |     113.91 ns |     1.914 ns |     1.697 ns |   0.1274 | 0.0007 |    1600 B |
-| Bunpo5Parse     |      79.79 ns |     1.348 ns |     1.261 ns |        - |      - |         - |
-| Bunpo300Parse   |   5,162.57 ns |    77.373 ns |    72.374 ns |        - |      - |         - |
-| FParseSetup     |     448.28 ns |     5.402 ns |     4.511 ns |   0.4959 | 0.0105 |    6232 B |
-| FParse5Parse    |     276.17 ns |     2.908 ns |     2.720 ns |   0.0324 |      - |     408 B |
-| FParse300Parse  |  16,797.90 ns |   201.356 ns |   178.496 ns |   1.6479 |      - |   20968 B |
-| SpracheSetup    |     164.69 ns |     3.281 ns |     2.908 ns |   0.1931 | 0.0017 |    2424 B |
-| Sprache5Parse   |   6,145.05 ns |    93.162 ns |    82.586 ns |   3.4332 | 0.0458 |   43144 B |
-| Sprache300Parse | 384,084.63 ns | 7,396.391 ns | 7,264.245 ns | 209.9609 | 7.8125 | 2634944 B |
+| BunpoSetup      |      88.15 ns |     0.741 ns |     0.657 ns |   0.1045 | 0.0005 |    1312 B |
+| Bunpo5Parse     |      78.28 ns |     0.621 ns |     0.550 ns |        - |      - |         - |
+| Bunpo300Parse   |   5,063.21 ns |    70.120 ns |    65.590 ns |        - |      - |         - |
+| FParseSetup     |     439.70 ns |     6.466 ns |     5.049 ns |   0.4964 | 0.0110 |    6232 B |
+| FParse5Parse    |     273.23 ns |     1.940 ns |     1.720 ns |   0.0324 |      - |     408 B |
+| FParse300Parse  |  16,480.95 ns |   158.952 ns |   148.684 ns |   1.6479 |      - |   20968 B |
+| SpracheSetup    |     164.50 ns |     2.630 ns |     2.196 ns |   0.1931 | 0.0017 |    2424 B |
+| Sprache5Parse   |   5,913.91 ns |    30.324 ns |    23.675 ns |   3.4332 | 0.0458 |   43144 B |
+| Sprache300Parse | 394,746.39 ns | 7,008.131 ns | 5,852.107 ns | 209.4727 | 7.8125 | 2631104 B |
+| ManualSetup     |      19.04 ns |     0.381 ns |     0.408 ns |   0.0191 |      - |     240 B |
+| Manual5Parse    |      49.17 ns |     0.186 ns |     0.165 ns |        - |      - |         - |
+| Manual300Parse  |   3,080.70 ns |    10.874 ns |     8.490 ns |        - |      - |         - |
 
 FParsec.CSharp version 12.6.0(FParsec version 2.0.0-beta2) used.  
 Sprache version 2.3.1 used.  
+Manual is LL parser written in C#.  
+[Benchmark source code](https://github.com/zenuas/Bunpo/blob/master/bench/StaticCalculationBench.cs)  
 
 ## Installation
 
--- bench/StaticCalculationBench.cs
+++ bench/StaticCalculationBench.cs
@@ -14,6 +14,7 @@ public class StaticCalculationBench
     public Func<string, int, (int, double)?> BunpoParser = null!;
     public Parser<double> SpracheParser = null!;
     public FSharpFunc<CharStream<Unit>, Reply<double>> FParseParser = null!;
+    public Func<ReadOnlySpan<char>, (int, double)?> ManualParser = null!;
 
     public const string Expr5 = "1-(2*3/4+5)";
 
@@ -34,6 +35,7 @@ public class StaticCalculationBench
         BunpoParser = BunpoSetup();
         SpracheParser = SpracheSetup();
         FParseParser = FParseSetup();
+        ManualParser = ManualSetup();
     }
 
     [Benchmark]
@@ -147,6 +149,94 @@ public class StaticCalculationBench
     public void Sprache300Parse()
     {
         var result = SpracheParser.Parse(Expr300);
+        if (result != 3853870d) throw new("");
+    }
+
+    [Benchmark]
+    public Func<ReadOnlySpan<char>, (int, double)?> ManualSetup()
+    {
+        var Number = (int, double)? (ReadOnlySpan<char> input) =>
+        {
+            if (input.Length == 0 || !char.IsAsciiDigit(input[0])) return null;
+            double value = input[0] - '0';
+            for (var i = 1; i < input.Length; i++)
+            {
+                if (!char.IsAsciiDigit(input[i])) return (i, value);
+                value = value * 10 + input[i] - '0';
+            }
+            return (input.Length, value);
+        };
+
+        Func<ReadOnlySpan<char>, (int, double)?> expr = null!;
+
+        var factor = (int, double)? (ReadOnlySpan<char> input) =>
+        {
+            if (Number(input) is { } num) return num;
+            if (input[0] == '(')
+            {
+                if (expr(input[1..]) is { } e && 1 + e.Item1 < input.Length && input[1 + e.Item1] == ')') return (2 + e.Item1, e.Item2);
+            }
+            return null;
+        };
+        var term = (int, double)? (ReadOnlySpan<char> input) =>
+        {
+            if (factor(input) is { } left)
+            {
+                while (left.Item1 < input.Length)
+                {
+                    if (input[left.Item1] == '*')
+                    {
+                        if (factor(input[(left.Item1 + 1)..]) is { } mul) left = (left.Item1 + 1 + mul.Item1, left.Item2 * mul.Item2);
+                        else break;
+                    }
+                    else if (input[left.Item1] == '/')
+                    {
+                        if (factor(input[(left.Item1 + 1)..]) is { } div) left = (left.Item1 + 1 + div.Item1, left.Item2 / div.Item2);
+                        else break;
+                    }
+                    else break;
+                }
+                return left;
+            }
+            return null;
+        };
+        expr = (int, double)? (ReadOnlySpan<char> input) =>
+        {
+            if (term(input) is { } left)
+            {
+                while (left.Item1 < input.Length)
+                {
+                    if (input[left.Item1] == '+')
+                    {
+                        if (term(input[(left.Item1 + 1)..]) is { } add) left = (left.Item1 + 1 + add.Item1, left.Item2 + add.Item2);
+                        else break;
+                    }
+                    else if (input[left.Item1] == '-')
+                    {
+                        if (term(input[(left.Item1 + 1)..]) is { } sub) left = (left.Item1 + 1 + sub.Item1, left.Item2 - sub.Item2);
+                        else break;
+                    }
+                    else break;
+                }
+                return left;
+            }
+            return null;
+        };
+
+        return expr;
+    }
+
+    [Benchmark]
+    public void Manual5Parse()
+    {
+        var result = ManualParser(Expr5.AsSpan())!.Value.Item2;
+        if (result != -5.5d) throw new("");
+    }
+
+    [Benchmark]
+    public void Manual300Parse()
+    {
+        var result = ManualParser(Expr300.AsSpan())!.Value.Item2;
         if (result != 3853870d) throw new("");
     }
 }
